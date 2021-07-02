9b5386ea545af4ba460d10c4330406a1b68665de
neslihanKaraman <86553049+neslihanKaraman@users.noreply.github.com>
2021-07-02 12:16:41 +0300

    Cubic bezier egrisi
    Cubic bezier eğrisi çizebilen control

---
+++ BezierEgrisi_5/BezierEgrisi_5/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace BezierEgrisi_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myBezierCurve1 = new BezierEgrisi_5.MyBezierCurve();
+            this.SuspendLayout();
+            // 
+            // myBezierCurve1
+            // 
+            this.myBezierCurve1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.myBezierCurve1.Location = new System.Drawing.Point(27, 26);
+            this.myBezierCurve1.Name = "myBezierCurve1";
+            this.myBezierCurve1.Pen_width = 5;
+            this.myBezierCurve1.Size = new System.Drawing.Size(816, 457);
+            this.myBezierCurve1.TabIndex = 0;
+            this.myBezierCurve1.Text = "myBezierCurve1";
+            this.myBezierCurve1.X1 = 10F;
+            this.myBezierCurve1.X2 = 45F;
+            this.myBezierCurve1.X3 = 55F;
+            this.myBezierCurve1.X4 = 274F;
+            this.myBezierCurve1.Y1 = 20F;
+            this.myBezierCurve1.Y2 = 67F;
+            this.myBezierCurve1.Y3 = 36F;
+            this.myBezierCurve1.Y4 = 30F;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 505);
+            this.Controls.Add(this.myBezierCurve1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyBezierCurve myBezierCurve1;
+    }
+}
+
---
+++ BezierEgrisi_5/BezierEgrisi_5/MyBezierCurve.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BezierEgrisi_5
+{
+    public partial class MyBezierCurve : Control
+    {
+        float a;
+
+        public float X1 { get; set; }
+        public float Y1 { get; set; }
+        public float X2 { get; set; }
+        public float Y2 { get; set; }
+        public float X3 { get; set; }
+        public float Y3 { get; set; }
+        public float X4 { get; set; }
+        public float Y4 { get; set; }
+        public int Pen_width { get; set; }
+
+        public MyBezierCurve()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Doğruluğunu kontrol etmek için Grafik sınıfının Bezier methodunu kullandım.
+        /// Kendi yazmış olduğum methoda parametreler aktardım.
+        /// </summary>
+        /// <param name="pe"></param>
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            //Pen pen = new Pen(Color.Black, 1);
+            //pe.Graphics.DrawBezier(pen, 10, 20, 45, 67, 55, 36, 74, 30);
+          
+            MyDrawBezier(new Pen(ForeColor,Pen_width), X1,Y1,X2,Y2,X3,Y3,X4,Y4);
+        }
+        /// <summary>
+        /// Bezier eğrisi çizdirmek için gerekli olan denklemi ifade ettim. Noktalar halinde yerlerini belirledim ve onları birleştirdim.
+        /// Daha sonra hafızada yer kaplamaması için ürettiğim nesneyi sildim 
+        /// </summary>
+        /// <param name="pen"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <param name="x3"></param>
+        /// <param name="y3"></param>
+        /// <param name="x4"></param>
+        /// <param name="y4"></param>
+        public void MyDrawBezier(Pen pen, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+        {
+            float t = 0F;
+            float u = 1 - t;
+            Graphics g = this.CreateGraphics();
+            a = 0.001f;
+            float baslangıc_x1 = Convert.ToSingle(Math.Pow(u, 3) * x1 + 3 * Math.Pow(u, 2) * t * x2 + 3 * u * Math.Pow(t, 2) * x3 + Math.Pow(t, 3) * x4);
+            float baslangıc_y1 = Convert.ToSingle(Math.Pow(u, 3) * y1 + 3 * Math.Pow(u, 2) * t * y2 + 3 * u * Math.Pow(t, 2) * y3 + Math.Pow(t, 3) * y4);
+
+            for (t = 0.001F; t <= 1.0F; t += a)
+            {
+                u = 1 - t;
+
+                float bx2 = Convert.ToSingle(Math.Pow(u, 3) * x1 + 3 * Math.Pow(u, 2) * t * x2 + 3 * u * Math.Pow(t, 2) * x3 + Math.Pow(t, 3) * x4);
+                float by2 = Convert.ToSingle(Math.Pow(u, 3) * y1 + 3 * Math.Pow(u, 2) * t * y2 + 3 * u * Math.Pow(t, 2) * y3 + Math.Pow(t, 3) * y4);
+
+                g.DrawLine(pen, baslangıc_x1, baslangıc_y1, bx2, by2);
+
+                baslangıc_x1 = bx2;
+                baslangıc_y1 = by2;
+            }
+            pen.Dispose();
+        }
+    }
+}
